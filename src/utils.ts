99fdd42cd144897764b997bd6e8bb113a9e69fa3
mariakanderson <mariakanderson@yahoo.com>
2025-01-13 13:16:41 +0000

    Document dependencies - removing deprecated function

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ import { SchematicsException, Tree } from '@angular-devkit/schematics';
 export function createHost(tree: Tree): workspaces.WorkspaceHost {
   return {
     async readFile(path: string): Promise<string> {
-      const data = tree.read(path);
+      let data = tree.read(path);
       if (!data) {
         throw new SchematicsException('File not found.');
       }
